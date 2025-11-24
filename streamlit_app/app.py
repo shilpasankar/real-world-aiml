fa08f8e1106ea8f21a8e32bbfa68233a274ed40c
Shilpa Sankar <shilpa.ayu@gmail.com>
2025-11-24 16:50:09 +0400

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Hello from Shilpa’s AI/ML Portfolio 👋")
-st.write("This is a test deployment from your GitHub repo.")
+st.write("This is the homepage of my multi-page Streamlit app.")
